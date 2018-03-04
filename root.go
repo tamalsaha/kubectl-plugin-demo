0dac7055cbec3ed7652b768ad8178d7e0118db01
tamal <tamal@appscode.com>
2018-03-04 14:12:51 -0800

    added-all

--- root.go
+++ root.go
@@ -13,9 +13,9 @@ import (
 )
 
 func NewRootCmd(plugin bool) *cobra.Command {
-	var (
-		enableAnalytics = true
-	)
+	//var (
+	//	enableAnalytics = true
+	//)
 	var rootCmd = &cobra.Command{
 		Use:               "kubectl-plugin-demo",
 		Short:             `Tamal's kubectl plugin'`,
@@ -30,29 +30,34 @@ func NewRootCmd(plugin bool) *cobra.Command {
 	clientConfig := util.DefaultClientConfig(flags)
 	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	if plugin {
-		flags.Set(clientcmd.FlagClusterName, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagClusterName, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagAuthInfoName, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagAuthInfoName, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagContext, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagContext, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagNamespace, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagNamespace, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagAPIServer, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagAPIServer, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagInsecure, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagInsecure, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagCertFile, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagCertFile, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagKeyFile, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagKeyFile, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagCAFile, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagCAFile, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagBearerToken, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagBearerToken, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagImpersonate, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagImpersonate, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagImpersonateGroup, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagImpersonateGroup, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagUsername, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagUsername, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagPassword, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagPassword, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
-		flags.Set(clientcmd.FlagTimeout, os.Getenv(plugins.FlagToEnvName(clientcmd.FlagTimeout, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagClusterName)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagAuthInfoName)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagContext)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagNamespace)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagAPIServer)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagInsecure)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagCertFile)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagKeyFile)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagCAFile)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagBearerToken)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagImpersonate)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagImpersonateGroup)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagUsername)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagPassword)
+		processKubectlFlag(rootCmd.PersistentFlags(), clientcmd.FlagTimeout)
 	}
 	// rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	// ref: https://github.com/kubernetes/kubernetes/issues/17162#issuecomment-225596212
 	flag.CommandLine.Parse([]string{})
-	rootCmd.PersistentFlags().BoolVar(&enableAnalytics, "analytics", enableAnalytics, "Send analytical events to Google Analytics")
+	// rootCmd.PersistentFlags().BoolVar(&enableAnalytics, "analytics", enableAnalytics, "Send analytical events to Google Analytics")
 
 	rootCmd.AddCommand(NewCmdListNodes(clientConfig))
 	rootCmd.AddCommand(NewCmdEnv())
 	rootCmd.AddCommand(NewCmdInstall(rootCmd))
 	return rootCmd
+}
+
+func processKubectlFlag(flags *pflag.FlagSet, name string) {
+	flags.Set(name, os.Getenv(plugins.FlagToEnvName(name, "KUBECTL_PLUGINS_GLOBAL_FLAG_")))
+	flags.MarkHidden(name)
 }
